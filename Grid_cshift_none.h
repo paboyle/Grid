526765874844641e3c0061ddc7145a8eeba0b44a
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-04-10 05:53:09 +0200

    Fixing the comms=none compile

--- Grid_cshift_none.h
+++ Grid_cshift_none.h
@@ -6,6 +6,7 @@ friend Lattice<vobj> Cshift(Lattice<vobj> &rhs,int dimension,int shift)
   Lattice<vobj> ret(rhs._grid);
   ret.checkerboard = rhs._grid->CheckerBoardDestination(rhs.checkerboard,shift);
   Cshift_local(ret,rhs,dimension,shift);
+  return ret;
 }
         
 #endif
