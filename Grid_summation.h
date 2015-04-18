1408a3c0f9fe3f117dd42008cc257991fc2e8e3a
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-04-18 12:17:13 +0100

    Got traceIndex, transposeIndex fully working. Need to think about peekIndex interface and () based indexing.

---
+++ Grid_summation.h
@@ -0,0 +1,12 @@
+#ifndef GRID_SUMMATION_H
+#define GRID_SUMMATION_H
+
+template<class vobj>
+inline void sumBlocks(Lattice<vobj> &coarseData,const Lattice<vobj &fineData)
+{
+  GridBase * fine  = findData._grid;
+  GridBase * coarse= findData._grid;
+  
+  return;
+}
+#endif
