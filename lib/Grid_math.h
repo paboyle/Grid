8ddfa7e6b0885ff316f14af6ab1d7b12d323516d
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-04-18 21:23:32 +0100

    Reorganisation

--- lib/Grid_math.h
+++ lib/Grid_math.h
@@ -1,16 +1,16 @@
 #ifndef GRID_MATH_H
 #define GRID_MATH_H
 
-#include <Grid_math_traits.h>
-#include <Grid_math_tensors.h>
-#include <Grid_math_arith.h>
-#include <Grid_math_inner.h>
-#include <Grid_math_outer.h>
-#include <Grid_math_transpose.h>
-#include <Grid_math_trace.h>
-#include <Grid_math_peek.h>
-#include <Grid_math_poke.h>
-#include <Grid_math_reality.h>
+#include <math/Grid_math_traits.h>
+#include <math/Grid_math_tensors.h>
+#include <math/Grid_math_arith.h>
+#include <math/Grid_math_inner.h>
+#include <math/Grid_math_outer.h>
+#include <math/Grid_math_transpose.h>
+#include <math/Grid_math_trace.h>
+#include <math/Grid_math_peek.h>
+#include <math/Grid_math_poke.h>
+#include <math/Grid_math_reality.h>
 
     
 #endif
