c8dc8ff89171ce86fc5cd5fe8be151ad02622722
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-05-11 18:59:03 +0100

    Adding a better controlled threading class, preparing to force in deterministic reduction.

--- tests/Grid_cshift.cc
+++ tests/Grid_cshift.cc
@@ -8,11 +8,10 @@ int main (int argc, char ** argv)
 {
   Grid_init(&argc,&argv);
 
-  std::vector<int> simd_layout;
-  std::vector<int> mpi_layout;
-  std::vector<int> latt_size;
+  std::vector<int> latt_size   = GridDefaultLatt();
+  std::vector<int> simd_layout = GridDefaultSimd();
+  std::vector<int> mpi_layout  = GridDefaultMpi();
 
-  GridParseLayout(argv,argc,latt_size,simd_layout,mpi_layout);
   GridCartesian        Fine(latt_size,simd_layout,mpi_layout);
 
   GridParallelRNG      FineRNG(&Fine);  FineRNG.SeedRandomDevice();
