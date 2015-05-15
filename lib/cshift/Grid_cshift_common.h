6b2a7867790c0bc988fc820e627cafc28986815d
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-05-15 11:36:22 +0100

    gcc doesn't like collapse(2) for some reason I can't figure

--- lib/cshift/Grid_cshift_common.h
+++ lib/cshift/Grid_cshift_common.h
@@ -185,14 +185,15 @@ template<class vobj> void Copy_plane_permute(Lattice<vobj>& lhs,Lattice<vobj> &r
   
 PARALLEL_NESTED_LOOP2
   for(int n=0;n<rhs._grid->_slice_nblock[dimension];n++){
-    for(int b=0;b<rhs._grid->_slice_block[dimension];b++){
-      int o =n*rhs._grid->_slice_stride[dimension];
+  for(int b=0;b<rhs._grid->_slice_block [dimension];b++){
+
+      int o  =n*rhs._grid->_slice_stride[dimension];
       int ocb=1<<lhs._grid->CheckerBoardFromOindex(o+b);
       if ( ocb&cbmask ) {
 	permute(lhs._odata[lo+o+b],rhs._odata[ro+o+b],permute_type);
       }
-    }
-  }
+
+  }}
 }
 
 //////////////////////////////////////////////////////
