519eab8ff0645986c25b75187fed95931d3dcac8
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-05-15 11:42:51 +0100

    More elegant enable_if

--- lib/math/Grid_math_poke.h
+++ lib/math/Grid_math_poke.h
@@ -7,23 +7,20 @@ namespace Grid {
 //////////////////////////////////////////////////////////////////////////////
 
 // Scalar poke
-template<int Level,class vtype> inline 
-  void pokeIndex(iScalar<vtype> &ret, 
-		 const typename std::enable_if<matchGridTensorIndex<iScalar<vtype>,Level>::value,iScalar<vtype> >::type &arg)
+template<int Level,class vtype,typename std::enable_if< iScalar<vtype>::TensorLevel == Level >::type * =nullptr> inline 
+  void pokeIndex(iScalar<vtype> &ret, const iScalar<vtype> &arg)
 {
   ret._internal = arg._internal;
 }
 // Vector poke, one index
-template<int Level,class vtype,int N> inline 
-  void pokeIndex(iVector<vtype,N> &ret, 
-		 const typename std::enable_if<matchGridTensorIndex<iVector<vtype,N>,Level>::value,iScalar<vtype> >::type &arg,int i)
+template<int Level,class vtype,int N,typename std::enable_if< iScalar<vtype>::TensorLevel == Level >::type * =nullptr> inline 
+  void pokeIndex(iVector<vtype,N> &ret, const iScalar<vtype> &arg,int i)
 {
   ret._internal[i] = arg._internal;
 }
 // Vector poke, two indices
-template<int Level,class vtype,int N> inline 
-  void pokeIndex(iMatrix<vtype,N> &ret, 
-		 const typename std::enable_if<matchGridTensorIndex<iMatrix<vtype,N>,Level>::value,iScalar<vtype> >::type &arg,int i,int j)
+template<int Level,class vtype,int N,typename std::enable_if< iScalar<vtype>::TensorLevel == Level >::type * =nullptr> inline 
+  void pokeIndex(iMatrix<vtype,N> &ret, const iScalar<vtype> &arg,int i,int j)
 {
   ret._internal[i][j] = arg._internal;
 }
@@ -32,55 +29,41 @@ template<int Level,class vtype,int N> inline
 // No match poke for scalar,vector,matrix must forward on either 0,1,2 args. Must have 9 routines with notvalue
 /////////////
 // scalar
-template<int Level,class vtype> inline 
-  void pokeIndex(iScalar<vtype> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iScalar<vtype>,Level>::notvalue,iScalar<decltype(peekIndex<Level>(ret._internal))> >::type &arg)
+template<int Level,class vtype,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+void pokeIndex(iScalar<vtype> &ret, const iScalar<decltype(peekIndex<Level>(ret._internal))>  &arg)
 		 
 {
   pokeIndex<Level>(ret._internal,arg._internal);
 }
-template<int Level,class vtype> inline 
-  void pokeIndex(iScalar<vtype> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iScalar<vtype>,Level>::notvalue,iScalar<decltype(peekIndex<Level>(ret._internal,0))> >::type &arg,
-		 int i)
+template<int Level,class vtype,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+  void pokeIndex(iScalar<vtype> &ret, const iScalar<decltype(peekIndex<Level>(ret._internal,0))> &arg, int i)
 		 
 {
   pokeIndex<Level>(ret._internal,arg._internal,i);
 }
-template<int Level,class vtype> inline 
-  void pokeIndex(iScalar<vtype> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iScalar<vtype>,Level>::notvalue,iScalar<decltype(peekIndex<Level>(ret._internal,0,0))> >::type &arg,
-		 int i,int j)
-		 
+template<int Level,class vtype,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+  void pokeIndex(iScalar<vtype> &ret, const iScalar<decltype(peekIndex<Level>(ret._internal,0,0))> &arg,int i,int j)
 {
   pokeIndex<Level>(ret._internal,arg._internal,i,j);
 }
 
 // Vector
-template<int Level,class vtype,int N> inline 
-  void pokeIndex(iVector<vtype,N> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iVector<vtype,N>,Level>::notvalue,iVector<decltype(peekIndex<Level>(ret._internal)),N> >::type &arg)
-		 
+template<int Level,class vtype,int N,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+  void pokeIndex(iVector<vtype,N> &ret, iVector<decltype(peekIndex<Level>(ret._internal)),N>  &arg)
 {
   for(int ii=0;ii<N;ii++){
     pokeIndex<Level>(ret._internal[ii],arg._internal[ii]);
   }
 }
-template<int Level,class vtype,int N> inline 
-  void pokeIndex(iVector<vtype,N> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iVector<vtype,N>,Level>::notvalue,iVector<decltype(peekIndex<Level>(ret._internal,0)),N> >::type &arg,
-		 int i)
-		 
+template<int Level,class vtype,int N,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+  void pokeIndex(iVector<vtype,N> &ret, const iVector<decltype(peekIndex<Level>(ret._internal,0)),N> &arg,int i)
 {
   for(int ii=0;ii<N;ii++){
     pokeIndex<Level>(ret._internal[ii],arg._internal[ii],i);
   }
 }
-template<int Level,class vtype,int N> inline 
-  void pokeIndex(iVector<vtype,N> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iVector<vtype,N>,Level>::notvalue,iVector<decltype(peekIndex<Level>(ret._internal,0,0)),N> >::type &arg,
-		 int i,int j)
-		 
+template<int Level,class vtype,int N,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+  void pokeIndex(iVector<vtype,N> &ret, const iVector<decltype(peekIndex<Level>(ret._internal,0,0)),N> &arg,int i,int j)
 {
   for(int ii=0;ii<N;ii++){
     pokeIndex<Level>(ret._internal[ii],arg._internal[ii],i,j);
@@ -88,32 +71,24 @@ template<int Level,class vtype,int N> inline
 }
 
 // Matrix
-template<int Level,class vtype,int N> inline 
-  void pokeIndex(iMatrix<vtype,N> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iMatrix<vtype,N>,Level>::notvalue,iMatrix<decltype(peekIndex<Level>(ret._internal)),N> >::type &arg)
-		 
+template<int Level,class vtype,int N,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+  void pokeIndex(iMatrix<vtype,N> &ret, const iMatrix<decltype(peekIndex<Level>(ret._internal)),N> &arg)		 
 {
   for(int ii=0;ii<N;ii++){
   for(int jj=0;jj<N;jj++){
     pokeIndex<Level>(ret._internal[ii][jj],arg._internal[ii][jj]);
   }}
 }
-template<int Level,class vtype,int N> inline 
-  void pokeIndex(iMatrix<vtype,N> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iMatrix<vtype,N>,Level>::notvalue,iMatrix<decltype(peekIndex<Level>(ret._internal,0)),N> >::type &arg,
-		 int i)
-		 
+template<int Level,class vtype,int N,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+  void pokeIndex(iMatrix<vtype,N> &ret, const iMatrix<decltype(peekIndex<Level>(ret._internal,0)),N> &arg,int i)
 {
   for(int ii=0;ii<N;ii++){
   for(int jj=0;jj<N;jj++){
     pokeIndex<Level>(ret._internal[ii][jj],arg._internal[ii][jj],i);
   }}
 }
-template<int Level,class vtype,int N> inline 
-  void pokeIndex(iMatrix<vtype,N> &ret,
-		 const typename std::enable_if<matchGridTensorIndex<iMatrix<vtype,N>,Level>::notvalue,iMatrix<decltype(peekIndex<Level>(ret._internal,0,0)),N> >::type &arg,
-		 int i,int j)
-		 
+template<int Level,class vtype,int N,typename std::enable_if< iScalar<vtype>::TensorLevel != Level >::type * =nullptr> inline 
+  void pokeIndex(iMatrix<vtype,N> &ret, const iMatrix<decltype(peekIndex<Level>(ret._internal,0,0)),N> &arg, int i,int j)
 {
   for(int ii=0;ii<N;ii++){
   for(int jj=0;jj<N;jj++){
