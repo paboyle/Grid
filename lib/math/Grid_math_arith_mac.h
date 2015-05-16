9f0e990b4054ee825b4eb4ab171748e5ad2f362e
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-05-16 04:36:22 +0100

    Optimisation and syntax pretty

--- lib/math/Grid_math_arith_mac.h
+++ lib/math/Grid_math_arith_mac.h
@@ -27,13 +27,14 @@ strong_inline  void mac(iScalar<rtype> * __restrict__ ret,const iScalar<vtype> *
 }
 template<class rrtype,class ltype,class rtype,int N>
 strong_inline void mac(iMatrix<rrtype,N> * __restrict__ ret,const iMatrix<ltype,N> * __restrict__ lhs,const iMatrix<rtype,N> * __restrict__ rhs){
-    for(int c2=0;c2<N;c2++){
-    for(int c1=0;c1<N;c1++){
     for(int c3=0;c3<N;c3++){
+    for(int c1=0;c1<N;c1++){
+    for(int c2=0;c2<N;c2++){
         mac(&ret->_internal[c1][c2],&lhs->_internal[c1][c3],&rhs->_internal[c3][c2]);
     }}}
     return;
 }
+
 template<class rrtype,class ltype,class rtype,int N>
 strong_inline void mac(iMatrix<rrtype,N> * __restrict__ ret,const iMatrix<ltype,N> * __restrict__ lhs,const iScalar<rtype> * __restrict__ rhs){
     for(int c1=0;c1<N;c1++){
