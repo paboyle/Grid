1d0df449e8a343fc3ac0b593bda054d3a1613f1c
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-06-03 12:47:05 +0100

    Reorganise of file naming

---
+++ lib/Cartesian.h
@@ -0,0 +1,8 @@
+#ifndef GRID_CARTESIAN_H
+#define GRID_CARTESIAN_H
+
+#include <cartesian/Cartesian_base.h>
+#include <cartesian/Cartesian_full.h>
+#include <cartesian/Cartesian_red_black.h> 
+
+#endif
