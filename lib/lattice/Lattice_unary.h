ddd93a0576bb4cdbf6599a7b70f3236790217848
Azusa Yamaguchi <ayamaguc@staffmail.ed.ac.uk>
2015-06-14 01:01:55 +0100

    Extra lattice unaries

--- lib/lattice/Lattice_unary.h
+++ lib/lattice/Lattice_unary.h
@@ -3,51 +3,6 @@
 
 namespace Grid {
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////////
-  //  avoid copy back routines for mult, mac, sub, add
-  //////////////////////////////////////////////////////////////////////////////////////////////////////
-  template<class obj> Lattice<obj> sqrt(const Lattice<obj> &rhs){
-    Lattice<obj> ret(rhs._grid);
-    ret.checkerboard = rhs.checkerboard;
-    conformable(ret,rhs);
-PARALLEL_FOR_LOOP
-    for(int ss=0;ss<rhs._grid->oSites();ss++){
-      ret._odata[ss]=sqrt(rhs._odata[ss]);
-    }
-    return ret;
-  }
-  template<class obj> Lattice<obj> rsqrt(const Lattice<obj> &rhs){
-    Lattice<obj> ret(rhs._grid);
-    ret.checkerboard = rhs.checkerboard;
-    conformable(ret,rhs);
-PARALLEL_FOR_LOOP
-    for(int ss=0;ss<rhs._grid->oSites();ss++){
-      ret._odata[ss]=rsqrt(rhs._odata[ss]);
-    }
-    return ret;
-  }
-  template<class obj> Lattice<obj> sin(const Lattice<obj> &rhs){
-    Lattice<obj> ret(rhs._grid);
-    ret.checkerboard = rhs.checkerboard;
-    conformable(ret,rhs);
-PARALLEL_FOR_LOOP
-    for(int ss=0;ss<rhs._grid->oSites();ss++){
-      ret._odata[ss]=sin(rhs._odata[ss]);
-    }
-    return ret;
-  }
-  template<class obj> Lattice<obj> cos(const Lattice<obj> &rhs){
-    Lattice<obj> ret(rhs._grid);
-    ret.checkerboard = rhs.checkerboard;
-    conformable(ret,rhs);
-PARALLEL_FOR_LOOP
-    for(int ss=0;ss<rhs._grid->oSites();ss++){
-      ret._odata[ss]=cos(rhs._odata[ss]);
-    }
-    return ret;
-  }
-
-
   template<class obj> Lattice<obj> pow(const Lattice<obj> &rhs,RealD y){
     Lattice<obj> ret(rhs._grid);
     ret.checkerboard = rhs.checkerboard;
