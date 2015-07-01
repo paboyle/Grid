84ec7c40cd7786dbe914559f1440ce473cf22cc6
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-07-01 23:43:57 +0100

    Cleaning up the recursion for traceIndex<n> after the changes the enable G++ to compile it again.

--- lib/Tensors.h
+++ lib/Tensors.h
@@ -8,11 +8,12 @@
 #include <tensors/Tensor_outer.h>
 #include <tensors/Tensor_transpose.h>
 #include <tensors/Tensor_trace.h>
+#include <tensors/Tensor_index.h>
 #include <tensors/Tensor_Ta.h>
 #include <tensors/Tensor_determinant.h>
 #include <tensors/Tensor_exp.h>
-#include <tensors/Tensor_peek.h>
-#include <tensors/Tensor_poke.h>
+//#include <tensors/Tensor_peek.h>
+//#include <tensors/Tensor_poke.h>
 #include <tensors/Tensor_reality.h>
 #include <tensors/Tensor_unary.h>
 #include <tensors/Tensor_extract_merge.h>
