cb9da7b371efc768a2b9b34eca29b2079880c04e
paboyle <paboyle@ph.ed.ac.uk>
2015-07-01 22:47:33 +0100

    Temporarily disable gmp dependency simply because Cray XC30's I'm benchmarking have a downlevel gmp version that chokes on ::max_align_t where gmp had a bug as far as I recall.

--- lib/algorithms/approx/Remez.h
+++ lib/algorithms/approx/Remez.h
@@ -15,7 +15,7 @@
 #ifndef INCLUDED_ALG_REMEZ_H
 #define INCLUDED_ALG_REMEZ_H
 
-#include <algorithms/approx/bigfloat.h>
+#include <algorithms/approx/bigfloat_double.h>
 
 #define JMAX 10000 //Maximum number of iterations of Newton's approximation
 #define SUM_MAX 10 // Maximum number of terms in exponential
