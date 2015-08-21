612957f0579faf6f8b2d2d846670f39395661e74
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-08-21 10:19:08 +0100

    pull in original license.

--- lib/serialisation/MacroMagic.h
+++ lib/serialisation/MacroMagic.h
@@ -18,6 +18,28 @@
 //
 // https://github.com/18sg/uSHET/blob/master/lib/cpp_magic.h
 //
+// The cpp_magic.h license (prior to my modifications):
+/*
+Copyright (c) 2014 Thomas Nixon, Jonathan Heathcote
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 ///////////////////////////////////////////
 
 #define strong_inline __attribute__((always_inline)) inline
@@ -81,6 +103,10 @@
 									)
 
 #define _GRID_MACRO_MAP() GRID_MACRO_MAP
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// These are the Grid extensions to serialisation (beyond popping first AND second)
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #define GRID_MACRO_MEMBER(A,B)        A B;
 
