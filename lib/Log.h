8709117aea51addcf6b8e9397b8ed04f14b68bbf
Antonin Portelli <antonin.portelli@me.com>
2015-10-27 17:31:13 +0000

    Log: generalised Logger class to allow separate logs in Grid-based applications

--- lib/Log.h
+++ lib/Log.h
@@ -6,31 +6,39 @@ namespace Grid {
 
 std::ostream& operator<< (std::ostream& stream, const GridTime& time);
 
-class GridLogger { 
-  int active;
-  std::string name;
+class Logger {
+protected:
+    int active;
+    std::string name, topName;
 public:
-
-  static GridStopWatch StopWatch;
-  static std::ostream devnull;
-  
-  GridLogger(int on, std::string nm): active(on), name(nm) { 
-  };
-  
-  void Active(int on) {active = on;};
-
-  friend std::ostream& operator<< (std::ostream& stream, const GridLogger& log){
-    if ( log.active ) {
-      StopWatch.Stop();
-      GridTime now = StopWatch.Elapsed();
-      StopWatch.Start();
-      stream << "Grid : "<<log.name << " : " << now << " : ";
-      return stream;
-    } else { 
-      return devnull;
+    static GridStopWatch StopWatch;
+    static std::ostream devnull;
+    
+    Logger(std::string topNm, int on, std::string nm)
+    : active(on), name(nm), topName(topNm) {};
+    
+    void Active(int on) {active = on;};
+    int  isActive(void) {return active;};
+    
+    friend std::ostream& operator<< (std::ostream& stream, const Logger& log){
+        if ( log.active ) {
+            StopWatch.Stop();
+            GridTime now = StopWatch.Elapsed();
+            StopWatch.Start();
+            stream << std::setw(8) << std::left << log.topName << " : ";
+            stream << std::setw(12) << std::left << log.name << " : ";
+            stream << now << " : ";
+            return stream;
+        } else { 
+            return devnull;
+        }
     }
-  }
-
+    
+};
+    
+class GridLogger: public Logger {
+public:
+  GridLogger(int on, std::string nm): Logger("Grid", on, nm){};
 };
 
 void GridLogConfigure(std::vector<std::string> &logstreams);
