96608c70d1364715e5e934f45051fd3d21cca683
Peter Boyle <paboyle@ph.ed.ac.uk>
2015-11-04 04:28:31 -0600

    chrono causing some problems on Cray systems. Suspend use for now

--- lib/parallelIO/NerscIO.h
+++ lib/parallelIO/NerscIO.h
@@ -83,7 +83,7 @@ inline void NerscMachineCharacteristics(NerscField &header)
   std::time_t t = std::time(nullptr);
   std::tm tm = *std::localtime(&t);
   std::ostringstream oss; 
-  oss << std::put_time(&tm, "%c %Z");
+  //  oss << std::put_time(&tm, "%c %Z");
   header.creation_date = oss.str();
   header.archive_date  = header.creation_date;
 
