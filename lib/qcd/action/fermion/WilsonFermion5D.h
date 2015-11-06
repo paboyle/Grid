1159de165ccd41a98368730ad5282847b60d905d
paboyle <paboyle@ph.ed.ac.uk>
2015-11-05 22:04:51 -0800

    Asm option for AVX512

--- lib/qcd/action/fermion/WilsonFermion5D.h
+++ lib/qcd/action/fermion/WilsonFermion5D.h
@@ -19,6 +19,7 @@ namespace Grid {
     class WilsonFermion5DStatic { 
     public:
       // S-direction is INNERMOST and takes no part in the parity.
+      static int AsmOptDslash; // these are a temporary hack
       static int HandOptDslash; // these are a temporary hack
       static const std::vector<int> directions;
       static const std::vector<int> displacements;
