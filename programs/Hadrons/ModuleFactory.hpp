fdd084859304d69cc456c142a7c6840cbe18694d
Antonin Portelli <antonin.portelli@me.com>
2016-02-25 12:07:21 +0000

    Hadrons: license text update

--- programs/Hadrons/ModuleFactory.hpp
+++ programs/Hadrons/ModuleFactory.hpp
@@ -1,21 +1,29 @@
-/*
- * ModuleFactory.hpp, part of Grid
- *
- * Copyright (C) 2015 Antonin Portelli
- *
- * Grid is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Grid is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Grid.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*******************************************************************************
+Grid physics library, www.github.com/paboyle/Grid 
+
+Source file: programs/Hadrons/ModuleFactory.hpp
+
+Copyright (C) 2015
+
+Author: Antonin Portelli <antonin.portelli@me.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+See the full license in the file "LICENSE" in the top level distribution 
+directory.
+*******************************************************************************/
 
 #ifndef Hadrons_ModuleFactory_hpp_
 #define Hadrons_ModuleFactory_hpp_
