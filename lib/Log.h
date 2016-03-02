68b02da483af6597a9a0eb9b3b685e5fa6851573
paboyle <paboyle@ph.ed.ac.uk>
2016-03-02 07:00:43 -0800

    Backing off the colour

--- lib/Log.h
+++ lib/Log.h
@@ -71,7 +71,8 @@ public:
             StopWatch.Start();
             stream << BLACK<< log.topName << BLACK<< " : ";
             stream << log.COLOUR <<std::setw(10) << std::left << log.name << BLACK << " : ";
-            stream << YELLOW<< now <<BLACK << " : " << log.COLOUR;
+	    //	    stream << YELLOW<< now <<BLACK << " : " ;
+	    stream << log.COLOUR;
             return stream;
         } else { 
             return devnull;
