629f43e36c14fe60eba53a651cafac7d3862a7fc
paboyle <paboyle@ph.ed.ac.uk>
2016-12-18 02:09:37 +0000

    Return statement needed

--- lib/simd/Grid_sse4.h
+++ lib/simd/Grid_sse4.h
@@ -192,7 +192,7 @@ namespace Optimization {
   struct MaddRealPart{
     inline __m128 operator()(__m128 a, __m128 b, __m128 c){
       __m128 ymm0 =  _mm_shuffle_ps(a,a,_MM_SELECT_FOUR_FOUR(2,2,0,0)); // ymm0 <- ar ar,
-      _mm_add_ps(_mm_mul_ps( ymm0, b),c);                         
+      return _mm_add_ps(_mm_mul_ps( ymm0, b),c);                         
     }
     inline __m128d operator()(__m128d a, __m128d b, __m128d c){
       __m128d ymm0 = _mm_shuffle_pd( a, a, 0x0 );
