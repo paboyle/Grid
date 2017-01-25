ef8d3831eb171e26de039e5d56df91f050301a3c
Guido Cossu <guido.cossu@ed.ac.uk>
2017-01-25 18:12:04 +0000

    Temporary patch the threading error in InsertSlice and ExtractSlice
    Find source and fix the error

--- lib/lattice/Lattice_transfer.h
+++ lib/lattice/Lattice_transfer.h
@@ -386,7 +386,8 @@ void InsertSlice(Lattice<vobj> &lowDim,Lattice<vobj> & higherDim,int slice, int 
   }
 
   // the above should guarantee that the operations are local
-  PARALLEL_FOR_LOOP
+  // Guido: check the threading here
+  //PARALLEL_FOR_LOOP
   for(int idx=0;idx<lg->lSites();idx++){
     std::vector<int> lcoor(nl);
     std::vector<int> hcoor(nh);
@@ -428,7 +429,7 @@ void ExtractSlice(Lattice<vobj> &lowDim, Lattice<vobj> & higherDim,int slice, in
     }
   }
   // the above should guarantee that the operations are local
-  PARALLEL_FOR_LOOP
+  //PARALLEL_FOR_LOOP
   for(int idx=0;idx<lg->lSites();idx++){
     std::vector<int> lcoor(nl);
     std::vector<int> hcoor(nh);
