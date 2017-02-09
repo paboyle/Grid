8b6a6c8236b64f0426cdb045cda07540eea65a41
Guido Cossu <guido.cossu@ed.ac.uk>
2017-02-09 16:20:24 +0000

    Resolving small merge conflict

--- benchmarks/Benchmark_dwf.cc
+++ benchmarks/Benchmark_dwf.cc
@@ -382,12 +382,6 @@ int main (int argc, char ** argv)
       } 
 
 
-      //    ref =  src - Gamma(Gamma::Algebra::GammaX)* src ; // 1+gamma_x
-      tmp = U[mu]*Cshift(src,mu+1,1);
-      for(int i=0;i<ref._odata.size();i++){
-	ref._odata[i]+= tmp._odata[i] + Gamma(Gmu[mu])*tmp._odata[i]; ;
-      }
-
 
     }
 
