33edde245d45e323275d78cd9e47e7b1502f348b
Chulwoo Jung <chulwoo@bnl.gov>
2017-03-12 23:02:42 -0400

    Changing Dminus(Dag) to use full vectors to work correctly

--- lib/qcd/action/fermion/CayleyFermion5D.cc
+++ lib/qcd/action/fermion/CayleyFermion5D.cc
@@ -57,10 +57,11 @@ void CayleyFermion5D<Impl>::Dminus(const FermionField &psi, FermionField &chi)
 {
   int Ls=this->Ls;
 
-  this->DW(psi,this->tmp(),DaggerNo);
+  FermionField tmp_f(this->FermionGrid());
+  this->DW(psi,tmp_f,DaggerNo);
 
   for(int s=0;s<Ls;s++){
-    axpby_ssp(chi,Coeff_t(1.0),psi,-cs[s],this->tmp(),s,s);// chi = (1-c[s] D_W) psi
+    axpby_ssp(chi,Coeff_t(1.0),psi,-cs[s],tmp_f,s,s);// chi = (1-c[s] D_W) psi
   }
 }
 
@@ -112,10 +113,11 @@ void CayleyFermion5D<Impl>::DminusDag(const FermionField &psi, FermionField &chi
 {
   int Ls=this->Ls;
 
-  this->DW(psi,this->tmp(),DaggerYes);
+  FermionField tmp_f(this->FermionGrid());
+  this->DW(psi,tmp_f,DaggerYes);
 
   for(int s=0;s<Ls;s++){
-    axpby_ssp(chi,Coeff_t(1.0),psi,-cs[s],this->tmp(),s,s);// chi = (1-c[s] D_W) psi
+    axpby_ssp(chi,Coeff_t(1.0),psi,-cs[s],tmp_f,s,s);// chi = (1-c[s] D_W) psi
   }
 }
 template<class Impl>  
