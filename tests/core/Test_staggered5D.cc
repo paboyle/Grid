358eb759952feef6c6d36339e1e23dcf48631e62
paboyle <paboyle@ph.ed.ac.uk>
2017-03-28 12:20:02 -0400

    Shorten loop

--- tests/core/Test_staggered5D.cc
+++ tests/core/Test_staggered5D.cc
@@ -153,7 +153,7 @@ int main (int argc, char ** argv)
   std::cout<<GridLogMessage<<"=========================================================="<<std::endl;
 
   std::cout<<GridLogMessage << "Calling Ds"<<std::endl;
-  int ncall=100000;
+  int ncall=1000;
   double t0=usecond();
   for(int i=0;i<ncall;i++){
     Ds.Dhop(src,result,0);
