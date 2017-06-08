1d0ca65e28e987746d1f5561774d51c9b77385cc
Azusa Yamaguchi <ayamaguc@staffmail.ed.ac.uk>
2017-06-08 22:21:50 +0100

    Move Gfix into utils

--- lib/Grid.h
+++ lib/Grid.h
@@ -41,6 +41,7 @@ Author: paboyle <paboyle@ph.ed.ac.uk>
 #include <Grid/GridCore.h>
 #include <Grid/GridQCDcore.h>
 #include <Grid/qcd/action/Action.h>
+#include <Grid/qcd/utils/GaugeFix.h>
 #include <Grid/qcd/smearing/Smearing.h>
 #include <Grid/qcd/hmc/HMC_aggregate.h>
 
