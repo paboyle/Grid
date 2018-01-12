e6f7a5a818844e7ecfbdd207f5e24ea78a75f262
paboyle <paboyle@ph.ed.ac.uk>
2018-01-12 23:28:01 +0000

    Namespace

--- lib/util/Init.h
+++ lib/util/Init.h
@@ -1,4 +1,4 @@
-    /*************************************************************************************
+/*************************************************************************************
 
     Grid physics library, www.github.com/paboyle/Grid
 
@@ -24,43 +24,43 @@ Author: paboyle <paboyle@ph.ed.ac.uk>
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
     See the full license in the file "LICENSE" in the top level distribution directory
-    *************************************************************************************/
-    /*  END LEGAL */
+*************************************************************************************/
+/*  END LEGAL */
 #ifndef GRID_INIT_H
 #define GRID_INIT_H
 
-namespace Grid {
+NAMESPACE_BEGIN(Grid);
 
-  void Grid_init(int *argc,char ***argv);
-  void Grid_finalize(void);
+void Grid_init(int *argc,char ***argv);
+void Grid_finalize(void);
 
-  // internal, controled with --handle
-  void Grid_sa_signal_handler(int sig,siginfo_t *si,void * ptr);
-  void Grid_debug_handler_init(void);
-  void Grid_quiesce_nodes(void);
-  void Grid_unquiesce_nodes(void);
+// internal, controled with --handle
+void Grid_sa_signal_handler(int sig,siginfo_t *si,void * ptr);
+void Grid_debug_handler_init(void);
+void Grid_quiesce_nodes(void);
+void Grid_unquiesce_nodes(void);
 
-  const std::vector<int> GridDefaultSimd(int dims,int nsimd);
-  const std::vector<int> &GridDefaultLatt(void);
-  const std::vector<int> &GridDefaultMpi(void);
-  const int              &GridThreads(void)  ;
-  void                    GridSetThreads(int t) ;
-  void GridLogTimestamp(int);
-  void GridLogLayout();
+const std::vector<int> GridDefaultSimd(int dims,int nsimd);
+const std::vector<int> &GridDefaultLatt(void);
+const std::vector<int> &GridDefaultMpi(void);
+const int              &GridThreads(void)  ;
+void                    GridSetThreads(int t) ;
+void GridLogTimestamp(int);
+void GridLogLayout();
 
-  // Common parsing chores
-  std::string GridCmdOptionPayload(char ** begin, char ** end, const std::string & option);
-  bool        GridCmdOptionExists(char** begin, char** end, const std::string& option);
-  std::string GridCmdVectorIntToString(const std::vector<int> & vec);
-  void GridCmdOptionCSL(std::string str,std::vector<std::string> & vec);
-  void GridCmdOptionIntVector(std::string &str,std::vector<int> & vec);
-
-
-  void GridParseLayout(char **argv,int argc,
-		       std::vector<int> &latt,
-		       std::vector<int> &simd,
-		       std::vector<int> &mpi);
+// Common parsing chores
+std::string GridCmdOptionPayload(char ** begin, char ** end, const std::string & option);
+bool        GridCmdOptionExists(char** begin, char** end, const std::string& option);
+std::string GridCmdVectorIntToString(const std::vector<int> & vec);
+void GridCmdOptionCSL(std::string str,std::vector<std::string> & vec);
+void GridCmdOptionIntVector(std::string &str,std::vector<int> & vec);
 
 
-};
+void GridParseLayout(char **argv,int argc,
+		     std::vector<int> &latt,
+		     std::vector<int> &simd,
+		     std::vector<int> &mpi);
+
+NAMESPACE_END(Grid);
+
 #endif
