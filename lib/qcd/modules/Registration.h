54a8ea93ec632071adce96be08153b70b3e8640f
paboyle <paboyle@ph.ed.ac.uk>
2018-01-14 22:20:42 +0000

    Namespace QCD gone

--- lib/qcd/modules/Registration.h
+++ lib/qcd/modules/Registration.h
@@ -25,48 +25,48 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 See the full license in the file "LICENSE" in the top level distribution
 directory
 *************************************************************************************/
-/*  END LEGAL */
+			   /*  END LEGAL */
 
 #ifndef MODULES_REGISTRATION_H
 #define MODULES_REGISTRATION_H
 
-// simplify with macros
+			   // simplify with macros
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Actions
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-typedef QCD::WilsonGModule<ImplementationPolicy> WilsonGMod;
-typedef QCD::SymanzikGModule<ImplementationPolicy> SymanzikGMod;
-typedef QCD::IwasakiGModule<ImplementationPolicy> IwasakiGMod;
-typedef QCD::DBW2GModule<ImplementationPolicy> DBW2GMod;
-typedef QCD::RBCGModule<ImplementationPolicy> RBCGMod;
-typedef QCD::PlaqPlusRectangleGModule<ImplementationPolicy> PlaqPlusRectangleGMod;
+			   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			   // Actions
+			   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+typedef WilsonGModule<ImplementationPolicy> WilsonGMod;
+typedef SymanzikGModule<ImplementationPolicy> SymanzikGMod;
+typedef IwasakiGModule<ImplementationPolicy> IwasakiGMod;
+typedef DBW2GModule<ImplementationPolicy> DBW2GMod;
+typedef RBCGModule<ImplementationPolicy> RBCGMod;
+typedef PlaqPlusRectangleGModule<ImplementationPolicy> PlaqPlusRectangleGMod;
 
-static Registrar<QCD::WilsonGMod,            HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __WGmodXMLInit("Wilson"); 
-static Registrar<QCD::SymanzikGMod,          HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __SymGmodXMLInit("Symanzik"); 
-static Registrar<QCD::IwasakiGMod,           HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __IwGmodXMLInit("Iwasaki"); 
-static Registrar<QCD::DBW2GMod,              HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __DBW2GmodXMLInit("DBW2"); 
-static Registrar<QCD::RBCGMod,               HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __RBCGmodXMLInit("RBC"); 
-static Registrar<QCD::PlaqPlusRectangleGMod, HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __PPRectGmodXMLInit("PlaqPlusRect"); 
+static Registrar<WilsonGMod,            HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __WGmodXMLInit("Wilson"); 
+static Registrar<SymanzikGMod,          HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __SymGmodXMLInit("Symanzik"); 
+static Registrar<IwasakiGMod,           HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __IwGmodXMLInit("Iwasaki"); 
+static Registrar<DBW2GMod,              HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __DBW2GmodXMLInit("DBW2"); 
+static Registrar<RBCGMod,               HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __RBCGmodXMLInit("RBC"); 
+static Registrar<PlaqPlusRectangleGMod, HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __PPRectGmodXMLInit("PlaqPlusRect"); 
 
 
 // FIXME more general implementation
-static Registrar<QCD::TwoFlavourFModule<FermionImplementationPolicy> ,      
-	HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __TwoFlavourFmodXMLInit("TwoFlavours"); 
-static Registrar<QCD::TwoFlavourRatioFModule<FermionImplementationPolicy> , 
-	HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __TwoFlavourRatioFmodXMLInit("TwoFlavoursRatio"); 
-static Registrar<QCD::TwoFlavourEOFModule<FermionImplementationPolicy> ,    
-	HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __TwoFlavourEOFmodXMLInit("TwoFlavoursEvenOdd"); 
-static Registrar<QCD::TwoFlavourRatioEOFModule<FermionImplementationPolicy>,
-	HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __TwoFlavourRatioEOFmodXMLInit("TwoFlavoursEvenOddRatio"); 
-static Registrar<QCD::OneFlavourFModule<FermionImplementationPolicy> ,      
-	HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __OneFlavourFmodXMLInit("OneFlavour"); 
-static Registrar<QCD::OneFlavourEOFModule<FermionImplementationPolicy> ,    
-	HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __OneFlavourEOFmodXMLInit("OneFlavourEvenOdd"); 
-static Registrar<QCD::OneFlavourRatioFModule<FermionImplementationPolicy> , 
-	HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __OneFlavourRatioFmodXMLInit("OneFlavourRatio"); 
-static Registrar<QCD::OneFlavourRatioEOFModule<FermionImplementationPolicy>,
-	HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __OneFlavourRatioEOFmodXMLInit("OneFlavourEvenOddRatio"); 
+static Registrar<TwoFlavourFModule<FermionImplementationPolicy> ,      
+		 HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __TwoFlavourFmodXMLInit("TwoFlavours"); 
+static Registrar<TwoFlavourRatioFModule<FermionImplementationPolicy> , 
+		 HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __TwoFlavourRatioFmodXMLInit("TwoFlavoursRatio"); 
+static Registrar<TwoFlavourEOFModule<FermionImplementationPolicy> ,    
+		 HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __TwoFlavourEOFmodXMLInit("TwoFlavoursEvenOdd"); 
+static Registrar<TwoFlavourRatioEOFModule<FermionImplementationPolicy>,
+		 HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __TwoFlavourRatioEOFmodXMLInit("TwoFlavoursEvenOddRatio"); 
+static Registrar<OneFlavourFModule<FermionImplementationPolicy> ,      
+		 HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __OneFlavourFmodXMLInit("OneFlavour"); 
+static Registrar<OneFlavourEOFModule<FermionImplementationPolicy> ,    
+		 HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __OneFlavourEOFmodXMLInit("OneFlavourEvenOdd"); 
+static Registrar<OneFlavourRatioFModule<FermionImplementationPolicy> , 
+		 HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __OneFlavourRatioFmodXMLInit("OneFlavourRatio"); 
+static Registrar<OneFlavourRatioEOFModule<FermionImplementationPolicy>,
+		 HMC_ActionModuleFactory<gauge_string, typename ImplementationPolicy::Field, Serialiser> > __OneFlavourRatioEOFmodXMLInit("OneFlavourEvenOddRatio"); 
 
 
 
@@ -78,10 +78,10 @@ static Registrar<QCD::OneFlavourRatioEOFModule<FermionImplementationPolicy>,
 // Now a specific registration with a fermion field
 // here must instantiate CG and CR for every new fermion field type (macro!!)
 
-static Registrar< ConjugateGradientModule<QCD::WilsonFermionR::FermionField>,   
-                  HMC_SolverModuleFactory<solver_string, QCD::WilsonFermionR::FermionField, Serialiser> > __CGWFmodXMLInit("ConjugateGradient"); 
-static Registrar< ConjugateResidualModule<QCD::WilsonFermionR::FermionField>,   
-                  HMC_SolverModuleFactory<solver_string, QCD::WilsonFermionR::FermionField, Serialiser> > __CRWFmodXMLInit("ConjugateResidual"); 
+static Registrar< ConjugateGradientModule<WilsonFermionR::FermionField>,   
+                  HMC_SolverModuleFactory<solver_string, WilsonFermionR::FermionField, Serialiser> > __CGWFmodXMLInit("ConjugateGradient"); 
+static Registrar< ConjugateResidualModule<WilsonFermionR::FermionField>,   
+                  HMC_SolverModuleFactory<solver_string, WilsonFermionR::FermionField, Serialiser> > __CRWFmodXMLInit("ConjugateResidual"); 
 
 // add the staggered, scalar versions here
 
@@ -90,11 +90,11 @@ static Registrar< ConjugateResidualModule<QCD::WilsonFermionR::FermionField>,
 // Fermion operators
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static Registrar< QCD::WilsonFermionModule<FermionImplementationPolicy>,   
+static Registrar< WilsonFermionModule<FermionImplementationPolicy>,   
                   HMC_FermionOperatorModuleFactory<fermionop_string, FermionImplementationPolicy, Serialiser> > __WilsonFOPmodXMLInit("Wilson"); 
-static Registrar< QCD::MobiusFermionModule<FermionImplementationPolicy>,   
+static Registrar< MobiusFermionModule<FermionImplementationPolicy>,   
                   HMC_FermionOperatorModuleFactory<fermionop_string, FermionImplementationPolicy, Serialiser> > __MobiusFOPmodXMLInit("Mobius");
-static Registrar< QCD::DomainWallFermionModule<FermionImplementationPolicy>,   
+static Registrar< DomainWallFermionModule<FermionImplementationPolicy>,   
                   HMC_FermionOperatorModuleFactory<fermionop_string, FermionImplementationPolicy, Serialiser> > __DWFOPmodXMLInit("DomainWall");
 
 
@@ -102,20 +102,18 @@ static Registrar< QCD::DomainWallFermionModule<FermionImplementationPolicy>,
 // Observables
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static Registrar<QCD::PlaquetteMod<ImplementationPolicy>, HMC_ObservablesModuleFactory<observable_string, typename ImplementationPolicy::Field, Serialiser> > __OBSPLmodXMLInit("Plaquette"); 
+static Registrar<PlaquetteMod<ImplementationPolicy>, HMC_ObservablesModuleFactory<observable_string, typename ImplementationPolicy::Field, Serialiser> > __OBSPLmodXMLInit("Plaquette"); 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Checkpointers
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static Registrar<QCD::BinaryCPModule<ImplementationPolicy>, HMC_CPModuleFactory<cp_string, ImplementationPolicy, Serialiser> > __CPBinarymodXMLInit("Binary");
-static Registrar<QCD::NerscCPModule<ImplementationPolicy> , HMC_CPModuleFactory<cp_string, ImplementationPolicy, Serialiser> > __CPNerscmodXMLInit("Nersc");
+static Registrar<BinaryCPModule<ImplementationPolicy>, HMC_CPModuleFactory<cp_string, ImplementationPolicy, Serialiser> > __CPBinarymodXMLInit("Binary");
+static Registrar<NerscCPModule<ImplementationPolicy> , HMC_CPModuleFactory<cp_string, ImplementationPolicy, Serialiser> > __CPNerscmodXMLInit("Nersc");
 
 #ifdef HAVE_LIME
-static Registrar<QCD::ILDGCPModule<ImplementationPolicy>  , HMC_CPModuleFactory<cp_string, ImplementationPolicy, Serialiser> > __CPILDGmodXMLInit("ILDG");
+static Registrar<ILDGCPModule<ImplementationPolicy>  , HMC_CPModuleFactory<cp_string, ImplementationPolicy, Serialiser> > __CPILDGmodXMLInit("ILDG");
 #endif
-
-
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Integrators
