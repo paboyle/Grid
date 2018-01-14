e04f61b1fa12edc338d5d6349de91f2cc93a8d89
paboyle <paboyle@ph.ed.ac.uk>
2018-01-14 23:14:46 +0000

    Namespace

--- lib/qcd/action/fermion/PartialFractionFermion5D.h
+++ lib/qcd/action/fermion/PartialFractionFermion5D.h
@@ -1,4 +1,4 @@
-    /*************************************************************************************
+/*************************************************************************************
 
     Grid physics library, www.github.com/paboyle/Grid 
 
@@ -24,78 +24,74 @@ Author: Peter Boyle <paboyle@ph.ed.ac.uk>
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
     See the full license in the file "LICENSE" in the top level distribution directory
-    *************************************************************************************/
-    /*  END LEGAL */
+*************************************************************************************/
+/*  END LEGAL */
 #ifndef  GRID_QCD_PARTIAL_FRACTION_H
 #define  GRID_QCD_PARTIAL_FRACTION_H
 
 #include <Grid/qcd/action/fermion/WilsonFermion5D.h>
 
-namespace Grid {
+NAMESPACE_BEGIN(Grid);
 
-  namespace QCD {
+template<class Impl>
+class PartialFractionFermion5D : public WilsonFermion5D<Impl>
+{
+public:
+  INHERIT_IMPL_TYPES(Impl);
 
-    template<class Impl>
-    class PartialFractionFermion5D : public WilsonFermion5D<Impl>
-    {
-    public:
-     INHERIT_IMPL_TYPES(Impl);
+  const int part_frac_chroma_convention=1;
 
-      const int part_frac_chroma_convention=1;
+  void   Meooe_internal(const FermionField &in, FermionField &out,int dag);
+  void   Mooee_internal(const FermionField &in, FermionField &out,int dag);
+  void   MooeeInv_internal(const FermionField &in, FermionField &out,int dag);
+  void   M_internal(const FermionField &in, FermionField &out,int dag);
 
-      void   Meooe_internal(const FermionField &in, FermionField &out,int dag);
-      void   Mooee_internal(const FermionField &in, FermionField &out,int dag);
-      void   MooeeInv_internal(const FermionField &in, FermionField &out,int dag);
-      void   M_internal(const FermionField &in, FermionField &out,int dag);
+  // override multiply
+  virtual RealD  M    (const FermionField &in, FermionField &out);
+  virtual RealD  Mdag (const FermionField &in, FermionField &out);
 
-      // override multiply
-      virtual RealD  M    (const FermionField &in, FermionField &out);
-      virtual RealD  Mdag (const FermionField &in, FermionField &out);
+  // half checkerboard operaions
+  virtual void   Meooe       (const FermionField &in, FermionField &out);
+  virtual void   MeooeDag    (const FermionField &in, FermionField &out);
+  virtual void   Mooee       (const FermionField &in, FermionField &out);
+  virtual void   MooeeDag    (const FermionField &in, FermionField &out);
+  virtual void   MooeeInv    (const FermionField &in, FermionField &out);
+  virtual void   MooeeInvDag (const FermionField &in, FermionField &out);
 
-      // half checkerboard operaions
-      virtual void   Meooe       (const FermionField &in, FermionField &out);
-      virtual void   MeooeDag    (const FermionField &in, FermionField &out);
-      virtual void   Mooee       (const FermionField &in, FermionField &out);
-      virtual void   MooeeDag    (const FermionField &in, FermionField &out);
-      virtual void   MooeeInv    (const FermionField &in, FermionField &out);
-      virtual void   MooeeInvDag (const FermionField &in, FermionField &out);
+  // force terms; five routines; default to Dhop on diagonal
+  virtual void MDeriv  (GaugeField &mat,const FermionField &U,const FermionField &V,int dag);
+  virtual void MoeDeriv(GaugeField &mat,const FermionField &U,const FermionField &V,int dag);
+  virtual void MeoDeriv(GaugeField &mat,const FermionField &U,const FermionField &V,int dag);
 
-      // force terms; five routines; default to Dhop on diagonal
-      virtual void MDeriv  (GaugeField &mat,const FermionField &U,const FermionField &V,int dag);
-      virtual void MoeDeriv(GaugeField &mat,const FermionField &U,const FermionField &V,int dag);
-      virtual void MeoDeriv(GaugeField &mat,const FermionField &U,const FermionField &V,int dag);
+  virtual void   Instantiatable(void) =0; // ensure no make-eee
 
-      virtual void   Instantiatable(void) =0; // ensure no make-eee
+  // Efficient support for multigrid coarsening
+  virtual void  Mdir (const FermionField &in, FermionField &out,int dir,int disp);
 
-      // Efficient support for multigrid coarsening
-      virtual void  Mdir (const FermionField &in, FermionField &out,int dir,int disp);
+  // Constructors
+  PartialFractionFermion5D(GaugeField &_Umu,
+			   GridCartesian         &FiveDimGrid,
+			   GridRedBlackCartesian &FiveDimRedBlackGrid,
+			   GridCartesian         &FourDimGrid,
+			   GridRedBlackCartesian &FourDimRedBlackGrid,
+			   RealD _mass,RealD M5,const ImplParams &p= ImplParams());
 
-      // Constructors
-      PartialFractionFermion5D(GaugeField &_Umu,
-			       GridCartesian         &FiveDimGrid,
-			       GridRedBlackCartesian &FiveDimRedBlackGrid,
-			       GridCartesian         &FourDimGrid,
-			       GridRedBlackCartesian &FourDimRedBlackGrid,
-			       RealD _mass,RealD M5,const ImplParams &p= ImplParams());
+protected:
 
-    protected:
+  virtual void SetCoefficientsTanh(Approx::zolotarev_data *zdata,RealD scale);
+  virtual void SetCoefficientsZolotarev(RealD zolo_hi,Approx::zolotarev_data *zdata);
 
-      virtual void SetCoefficientsTanh(Approx::zolotarev_data *zdata,RealD scale);
-      virtual void SetCoefficientsZolotarev(RealD zolo_hi,Approx::zolotarev_data *zdata);
+  // Part frac
+  RealD mass;
+  RealD dw_diag;
+  RealD R;
+  RealD amax;
+  RealD scale;
+  std::vector<double> p; 
+  std::vector<double> q;
 
-      // Part frac
-      RealD mass;
-      RealD dw_diag;
-      RealD R;
-      RealD amax;
-      RealD scale;
-      std::vector<double> p; 
-      std::vector<double> q;
+};
 
-    };
-
-
-  }
-}
+NAMESPACE_END(Grid);
 
 #endif
