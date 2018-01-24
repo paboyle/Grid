c851b39a4907f546a2589b72d667115fd984005a
paboyle <paboyle@ph.ed.ac.uk>
2018-01-24 13:33:34 +0000

    Nicer way of including aggregate

--- lib/lattice/Lattice.h
+++ lib/lattice/Lattice.h
@@ -25,9 +25,22 @@ Author: Peter Boyle <paboyle@ph.ed.ac.uk>
     See the full license in the file "LICENSE" in the top level distribution directory
     *************************************************************************************/
     /*  END LEGAL */
-#ifndef GRID_LATTICE_H
-#define GRID_LATTICE_H
-
+#pragma once
 #include <Grid/lattice/Lattice_base.h>
+#include <Grid/lattice/Lattice_conformable.h>
+#include <Grid/lattice/Lattice_ET.h>
+#include <Grid/lattice/Lattice_arith.h>
+#include <Grid/lattice/Lattice_trace.h>
+#include <Grid/lattice/Lattice_transpose.h>
+#include <Grid/lattice/Lattice_local.h>
+#include <Grid/lattice/Lattice_reduction.h>
+#include <Grid/lattice/Lattice_peekpoke.h>
+#include <Grid/lattice/Lattice_reality.h>
+#include <Grid/lattice/Lattice_comparison_utils.h>
+#include <Grid/lattice/Lattice_comparison.h>
+#include <Grid/lattice/Lattice_coordinate.h>
+#include <Grid/lattice/Lattice_where.h>
+#include <Grid/lattice/Lattice_rng.h>
+#include <Grid/lattice/Lattice_unary.h>
+#include <Grid/lattice/Lattice_transfer.h>
 
-#endif
