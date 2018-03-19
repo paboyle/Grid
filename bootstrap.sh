7467a1c0277bdd259a0e0afeea50990ad3b15130
Peter Boyle <paboyle@ph.ed.ac.uk>
2018-03-19 07:08:10 -0400

    Latest eigen needed for GPU

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,10 @@
 EIGEN_URL='http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2'
 
 echo "-- deploying Eigen source..."
-wget ${EIGEN_URL} --no-check-certificate && ./scripts/update_eigen.sh `basename ${EIGEN_URL}` && rm `basename ${EIGEN_URL}`
+cd lib
+git clone https://github.com/eigenteam/eigen-git-mirror.git
+cd ..
+#wget ${EIGEN_URL} --no-check-certificate && ./scripts/update_eigen.sh `basename ${EIGEN_URL}` && rm `basename ${EIGEN_URL}`
 
 echo '-- generating Make.inc files...'
 ./scripts/filelist
