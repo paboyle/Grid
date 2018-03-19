a5cfb893043a4b406b6ee51d732d354c3ed9bc22
Peter Boyle <paboyle@ph.ed.ac.uk>
2018-03-19 07:20:48 -0400

    Update eigen process direct from develop on github. Dangerous, but needed from GPU

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,11 @@ EIGEN_URL='http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2'
 
 echo "-- deploying Eigen source..."
 cd lib
+rm -rf Eigen
 git clone https://github.com/eigenteam/eigen-git-mirror.git
+echo 'eigen_files =\' > Eigen.inc
+find Eigen -type f -print | sed 's/^/  /;$q;s/$/ \\/' >> Eigen.inc
 cd ..
-#wget ${EIGEN_URL} --no-check-certificate && ./scripts/update_eigen.sh `basename ${EIGEN_URL}` && rm `basename ${EIGEN_URL}`
 
 echo '-- generating Make.inc files...'
 ./scripts/filelist
