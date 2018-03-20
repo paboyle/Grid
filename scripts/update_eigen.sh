9c25eb35ca3bc9d758751b8342c858e6673446cc
Peter Boyle <paboyle@ph.ed.ac.uk>
2018-03-20 07:18:56 -0400

    Eigen develop branch for now

--- scripts/update_eigen.sh
+++ scripts/update_eigen.sh
@@ -7,13 +7,8 @@ fi
 ARC=$1
 
 INITDIR=`pwd`
-rm -rf lib/Eigen
-ARCDIR=`tar -tf ${ARC} | head -n1 | sed -e 's@/.*@@'`
-tar -xf ${ARC}
-cd ${ARCDIR}
-(tar -cf - Eigen --exclude='*.txt' 2>/dev/null) | tar -xf - -C ../lib/
 cd ../lib
 echo 'eigen_files =\' > Eigen.inc
 find Eigen -type f -print | sed 's/^/  /;$q;s/$/ \\/' >> Eigen.inc
 cd ${INITDIR}
-rm -rf ${ARCDIR}
+
