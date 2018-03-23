5f8225461b51ae27587a7a25685e9c823ee682f6
Guido Cossu <guido.cossu@ed.ac.uk>
2018-03-23 10:37:58 +0000

    Fencing mixedcg test propagator write. LIME is still optional in Grid

--- tests/Test_dwf_mixedcg_prec.cc
+++ tests/Test_dwf_mixedcg_prec.cc
@@ -103,6 +103,7 @@ int main (int argc, char ** argv)
 
   std::cout << "Diff between mixed and regular CG: " << diff << std::endl;
 
+  #ifdef HAVE_LIME
   std::string file1("./Propagator1");
   std::string file2("./Propagator2");
   emptyUserRecord record;
@@ -124,6 +125,8 @@ int main (int argc, char ** argv)
 						   nersc_csum,scidac_csuma,scidac_csumb);
 
   std::cout << " CG checksums "<<std::hex << scidac_csuma << " "<<scidac_csumb<<std::endl;
+  #endif
+
 
   Grid_finalize();
 }
