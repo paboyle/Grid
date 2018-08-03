fa5dee76b10b5d3f9096e4c83f49f3973f1e2ebf
fionnoh <fionnoh@gmail.com>
2018-08-03 15:15:54 +0100

    Included Peter's A2AMeson field and Eigen changes

--- extras/Hadrons/Modules/MContraction/A2AMeson.cc
+++
@@ -1,8 +0,0 @@
-#include <Grid/Hadrons/Modules/MContraction/A2AMeson.hpp>
-
-using namespace Grid;
-using namespace Hadrons;
-using namespace MContraction;
-
-template class Grid::Hadrons::MContraction::TA2AMeson<FIMPL>;
-template class Grid::Hadrons::MContraction::TA2AMeson<ZFIMPL>;
