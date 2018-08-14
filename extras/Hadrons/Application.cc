7727e811133fb1ad751c08d5d8a7dfe043a1ce36
Antonin Portelli <antonin.portelli@me.com>
2018-08-14 20:18:47 +0100

    Hadrons: slight improvement on previous commit

--- extras/Hadrons/Application.cc
+++ extras/Hadrons/Application.cc
@@ -110,14 +110,14 @@ void Application::run(void)
     LOG(Message) << "RUN ID '" << getPar().runId << "'" << std::endl;
     vm().setRunId(getPar().runId);
     vm().printContent();
+    env().printContent();
+    schedule();
+    printSchedule();
     if (!getPar().graphFile.empty())
     {
         makeFileDir(getPar().graphFile, env().getGrid());
         vm().dumpModuleGraph(getPar().graphFile);
     }
-    env().printContent();
-    schedule();
-    printSchedule();
     configLoop();
 }
 
