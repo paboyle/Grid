84189867ef5257df5e93b6a0bce333609623f001
Antonin Portelli <antonin.portelli@me.com>
2018-10-02 13:05:05 +0100

    Hadrons: eigenpack converter with RB grids (to be generalised)

--- Hadrons/Utilities/EigenPackCast.cc
+++ Hadrons/Utilities/EigenPackCast.cc
@@ -16,15 +16,17 @@ void convert(const std::string outFilename, const std::string inFilename,
     typedef typename FOut::vector_type VTypeOut;
     typedef typename FIn::vector_type  VTypeIn;
 
-    std::shared_ptr<GridBase> gIn, gOut;
+    std::shared_ptr<GridBase> fgIn, fgOut, gIn, gOut;
     auto                      dim     = GridDefaultLatt();
     unsigned int              nd      = dim.size();
     auto                      simdOut = GridDefaultSimd(nd, VTypeOut::Nsimd());
     auto                      simdIn  = GridDefaultSimd(nd, VTypeIn::Nsimd());
 
-    gOut.reset(SpaceTimeGrid::makeFourDimGrid(dim, simdOut, GridDefaultMpi()));
-    gIn.reset(SpaceTimeGrid::makeFourDimGrid(dim, simdIn, GridDefaultMpi()));
-
+    fgOut.reset(SpaceTimeGrid::makeFourDimGrid(dim, simdOut, GridDefaultMpi()));
+    gOut.reset(SpaceTimeGrid::makeFourDimRedBlackGrid(fgOut.get()));
+    fgIn.reset(SpaceTimeGrid::makeFourDimGrid(dim, simdIn, GridDefaultMpi()));
+    gIn.reset(SpaceTimeGrid::makeFourDimRedBlackGrid(fgOut.get()));
+    
     FOut bufOut(gOut.get());
     FIn  bufIn(gIn.get()), testIn(gIn.get());
 
