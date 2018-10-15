291bc2a1f086925b3778aac73dc0f47f0b0543fb
Antonin Portelli <antonin.portelli@me.com>
2018-10-15 17:25:08 +0100

    IO benchmark on a list of directories

--- benchmarks/Benchmark_IO.cc
+++ benchmarks/Benchmark_IO.cc
@@ -1,108 +1,48 @@
-#include <Grid/Grid.h>
-#ifdef HAVE_LIME
 
-using namespace std;
-using namespace Grid;
-using namespace Grid::QCD;
+#include "Benchmark_IO.hpp"
 
-#define MSG cout << GridLogMessage
-#define SEP \
-"============================================================================="
 #ifndef BENCH_IO_LMAX
 #define BENCH_IO_LMAX 40
 #endif
 
-typedef function<void(const string, LatticeFermion &)> WriterFn;
-typedef function<void(LatticeFermion &, const string)> ReaderFn;
+using namespace Grid;
+using namespace QCD;
 
-string filestem(const int l)
+std::string filestem(const int l)
 {
-  return "iobench_l" + to_string(l);
-}
-
-void limeWrite(const string filestem, LatticeFermion &vec)
-{
-  emptyUserRecord record;
-  ScidacWriter    binWriter(vec._grid->IsBoss());
-
-  binWriter.open(filestem + ".bin");
-  binWriter.writeScidacFieldRecord(vec, record);
-  binWriter.close();
-}
-
-void limeRead(LatticeFermion &vec, const string filestem)
-{
-  emptyUserRecord record;
-  ScidacReader    binReader;
-
-  binReader.open(filestem + ".bin");
-  binReader.readScidacFieldRecord(vec, record);
-  binReader.close();
-}
-
-void writeBenchmark(const int l, const WriterFn &write)
-{
-  auto                      mpi  = GridDefaultMpi();
-  auto                      simd = GridDefaultSimd(Nd, vComplex::Nsimd());
-  vector<int>               latt = {l*mpi[0], l*mpi[1], l*mpi[2], l*mpi[3]};
-  unique_ptr<GridCartesian> gPt(SpaceTimeGrid::makeFourDimGrid(latt, simd, mpi));
-  GridCartesian             *g = gPt.get();
-  GridParallelRNG           rng(g);
-  LatticeFermion            vec(g);
-  emptyUserRecord           record;
-  ScidacWriter              binWriter(g->IsBoss());
-
-  cout << "-- Local volume " << l << "^4" << endl;
-  random(rng, vec);
-  write(filestem(l), vec);
-}
-
-void readBenchmark(const int l, const ReaderFn &read)
-{
-  auto                      mpi  = GridDefaultMpi();
-  auto                      simd = GridDefaultSimd(Nd, vComplex::Nsimd());
-  vector<int>               latt = {l*mpi[0], l*mpi[1], l*mpi[2], l*mpi[3]};
-  unique_ptr<GridCartesian> gPt(SpaceTimeGrid::makeFourDimGrid(latt, simd, mpi));
-  GridCartesian             *g = gPt.get();
-  LatticeFermion            vec(g);
-  emptyUserRecord           record;
-  ScidacReader              binReader;
-
-  cout << "-- Local volume " << l << "^4" << endl;
-  read(vec, filestem(l));
+  return "iobench_l" + std::to_string(l);
 }
 
 int main (int argc, char ** argv)
 {
   Grid_init(&argc,&argv);
 
-  auto simd = GridDefaultSimd(Nd,vComplex::Nsimd());
-  auto mpi  = GridDefaultMpi();
-
   int64_t threads = GridThread::GetThreads();
-  MSG << "Grid is setup to use " << threads << " threads" << endl;
-  MSG << SEP << endl;
-  MSG << "Benchmark Lime write" << endl;
-  MSG << SEP << endl;
+  MSG << "Grid is setup to use " << threads << " threads" << std::endl;
+  MSG << SEP << std::endl;
+  MSG << "Benchmark Lime write" << std::endl;
+  MSG << SEP << std::endl;
   for (int l = 4; l <= BENCH_IO_LMAX; l += 2)
   {
-    writeBenchmark(l, limeWrite);
+    auto             mpi  = GridDefaultMpi();
+    std::vector<int> latt = {l*mpi[0], l*mpi[1], l*mpi[2], l*mpi[3]};
+
+    std::cout << "-- Local volume " << l << "^4" << std::endl;
+    writeBenchmark<LatticeFermion>(latt, filestem(l), limeWrite<LatticeFermion>);
   }
 
-  MSG << "Benchmark Lime read" << endl;
-  MSG << SEP << endl;
+  MSG << "Benchmark Lime read" << std::endl;
+  MSG << SEP << std::endl;
   for (int l = 4; l <= BENCH_IO_LMAX; l += 2)
   {
-    readBenchmark(l, limeRead);
+    auto             mpi  = GridDefaultMpi();
+    std::vector<int> latt = {l*mpi[0], l*mpi[1], l*mpi[2], l*mpi[3]};
+
+    std::cout << "-- Local volume " << l << "^4" << std::endl;
+    readBenchmark<LatticeFermion>(latt, filestem(l), limeRead<LatticeFermion>);
   }
 
   Grid_finalize();
 
   return EXIT_SUCCESS;
 }
-#else
-int main (int argc, char ** argv)
-{
-  return EXIT_SUCCESS;
-}
-#endif
---
+++ benchmarks/Benchmark_IO_vs_dir.cc
@@ -0,0 +1,63 @@
+#include "Benchmark_IO.hpp"
+
+#define MSG std::cout << GridLogMessage
+#define SEP \
+"============================================================================="
+#ifndef BENCH_IO_LMAX
+#define BENCH_IO_LMAX 40
+#endif
+
+using namespace Grid;
+using namespace QCD;
+
+int main (int argc, char ** argv)
+{
+  std::vector<std::string> dir;
+  unsigned int             Ls;
+  bool                     rb;
+  if (argc < 4)
+  {
+    std::cerr << "usage: " << argv[0] << " <Ls> <RB {0|1}> <dir1> [<dir2> ... <dirn>] [Grid options]";
+    std::cerr << std::endl;
+  }
+  Ls = std::stoi(argv[1]);
+  rb = (std::string(argv[2]) == "1");
+  for (unsigned int i = 3; i < argc; ++i)
+  {
+    std::string a = argv[i];
+
+    if (a[0] != '-')
+    {
+      dir.push_back(std::string(argv[i]));
+    }
+    else
+    {
+      break;
+    }
+  }
+  Grid_init(&argc,&argv);
+
+
+  int64_t threads = GridThread::GetThreads();
+  MSG << "Grid is setup to use " << threads << " threads" << std::endl;
+  MSG << SEP << std::endl;
+  MSG << "Benchmark Lime write" << std::endl;
+  MSG << SEP << std::endl;
+  for (auto &d: dir)
+  {
+    MSG << "-- Directory " << d << std::endl;
+    writeBenchmark<LatticeFermion>(GridDefaultLatt(), d + "/ioBench", limeWrite<LatticeFermion>, Ls, rb);
+  }
+
+  MSG << "Benchmark Lime read" << std::endl;
+  MSG << SEP << std::endl;
+  for (auto &d: dir)
+  {
+    MSG << "-- Directory " << d << std::endl;
+    readBenchmark<LatticeFermion>(GridDefaultLatt(), d + "/ioBench", limeRead<LatticeFermion>, Ls, rb);
+  }
+
+  Grid_finalize();
+
+  return EXIT_SUCCESS;
+}
