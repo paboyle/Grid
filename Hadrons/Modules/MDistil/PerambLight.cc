be5605931c45034626d8b587c255124d4597bfe4
ferben <ferben@localhost.localdomain>
2019-01-23 10:51:09 +0000

    merge

--- Hadrons/Modules/MDistil/PerambLight.cc
+++ Hadrons/Modules/MDistil/PerambLight.cc
@@ -4,4 +4,4 @@ using namespace Grid;
 using namespace Hadrons;
 using namespace MDistil;
 
-template class Grid::Hadrons::MDistil::Tperambulator_l<FIMPL>;
+template class Grid::Hadrons::MDistil::TPerambLight<FIMPL>;
