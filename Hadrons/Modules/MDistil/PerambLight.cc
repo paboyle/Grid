a9848becb008534f75855a6ac9f92ccc0cb9405e
ferben <ferben@localhost.localdomain>
2019-02-01 13:23:42 +0000

    unsmeared sinks can now be computed - new test program available

--- Hadrons/Modules/MDistil/PerambLight.cc
+++ Hadrons/Modules/MDistil/PerambLight.cc
@@ -4,4 +4,4 @@ using namespace Grid;
 using namespace Hadrons;
 using namespace MDistil;
 
-template class Grid::Hadrons::MDistil::TPerambLight<GIMPL>;
+template class Grid::Hadrons::MDistil::TPerambLight<FIMPL>;
