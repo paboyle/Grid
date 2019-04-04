25e4ee3a494247ac4cbb0ecf571b27f5a484cef5
Felix Erben <dc-erbe1@tesseract-login1.ib0.sgi.cluster.dirac.ed.ac.uk>
2019-04-04 12:13:16 +0100

    3D Stout smearing added

---
+++ Hadrons/Modules/MGauge/StoutSmearing3D.cc
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MGauge/StoutSmearing3D.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MGauge;
+
+template class Grid::Hadrons::MGauge::TStoutSmearing3D<GIMPL>;
