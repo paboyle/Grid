4aa426c97fdec810841ebe0e3b3cf627903535ea
fionnoh <fionnoh@gmail.com>
2019-05-03 11:49:38 +0100

    Andrew's archived Ward Identity module added back into Hadrons with results now output to file

---
+++ Hadrons/Modules/MContraction/WardIdentity.cc
@@ -0,0 +1,8 @@
+#include <Hadrons/Modules/MContraction/WardIdentity.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MContraction;
+
+template class Grid::Hadrons::MContraction::TWardIdentity<FIMPL>;
+template class Grid::Hadrons::MContraction::TWardIdentity<ZFIMPL>;
