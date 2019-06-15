9a13d2992c7d1a92d42222e8f85f2bdc83610ed3
Peter Boyle <paboyle@ph.ed.ac.uk>
2019-06-15 09:05:16 +0100

    lean up

--- Grid/qcd/action/fermion/implementation/StaggeredKernelsAsm.h
+++ Grid/qcd/action/fermion/implementation/StaggeredKernelsAsm.h
@@ -958,26 +958,5 @@ template <> void StaggeredKernels<StaggeredImplD>::DhopSiteAsm(StencilImpl &st, 
 #endif
 }
 
-extern template void StaggeredKernels<StaggeredVec5dImplF>::DhopSiteAsm(StencilImpl &st, LebesgueOrder &lo, 
-									DoubledGaugeFieldView &U,
-									DoubledGaugeFieldView &UUU,
-									SiteSpinor *buf, int LLs,
-									int sU, const FermionFieldView &in, FermionFieldView &out,int dag);
-extern template void StaggeredKernels<StaggeredVec5dImplD>::DhopSiteAsm(StencilImpl &st, LebesgueOrder &lo, 
-									DoubledGaugeFieldView &U,
-									DoubledGaugeFieldView &UUU,
-									SiteSpinor *buf, int LLs,
-									int sU, const FermionFieldView &in, FermionFieldView &out,int dag);
-extern template void StaggeredKernels<StaggeredImplF>::DhopSiteAsm(StencilImpl &st, LebesgueOrder &lo, 
-								   DoubledGaugeFieldView &U,
-								   DoubledGaugeFieldView &UUU,
-								   SiteSpinor *buf, int LLs,
-								   int sU, const FermionFieldView &in, FermionFieldView &out,int dag);
-extern template void StaggeredKernels<StaggeredImplD>::DhopSiteAsm(StencilImpl &st, LebesgueOrder &lo, 
-								   DoubledGaugeFieldView &U,
-								   DoubledGaugeFieldView &UUU,
-								   SiteSpinor *buf, int LLs,
-								   int sU, const FermionFieldView &in, FermionFieldView &out,int dag);
-
 NAMESPACE_END(Grid);
 
