c1bccb615f27442bf740762ed07d729d2ddebda6
Peter Georg <peter.georg@physik.uni-regensburg.de>
2019-06-21 14:17:18 +0200

    Fix ambigious function calls

--- Grid/tensors/Tensor_arith_scalar.h
+++ Grid/tensors/Tensor_arith_scalar.h
@@ -37,249 +37,103 @@ namespace Grid {
 //////////////////////////////////////////////////////////////////////////////////////////
 
 // multiplication by fundamental scalar type
-template<class l> strong_inline iScalar<l> operator * (const iScalar<l>& lhs,const typename iScalar<l>::scalar_type rhs) 
-{
-  typename iScalar<l>::tensor_reduced srhs; srhs=rhs;
-  return lhs*srhs;
-}
-template<class l> strong_inline iScalar<l> operator * (const typename iScalar<l>::scalar_type lhs,const iScalar<l>& rhs) {  return rhs*lhs; }
-
-template<class l,int N> strong_inline iVector<l,N> operator * (const iVector<l,N>& lhs,const typename iScalar<l>::scalar_type rhs) 
-{
-  typename iVector<l,N>::tensor_reduced srhs; srhs=rhs;
-  return lhs*srhs;
-}
-template<class l,int N> strong_inline iVector<l,N> operator * (const typename iScalar<l>::scalar_type lhs,const iVector<l,N>& rhs) {  return rhs*lhs; }
-
-template<class l,int N> strong_inline iMatrix<l,N> operator * (const iMatrix<l,N>& lhs,const typename iScalar<l>::scalar_type &rhs) 
-{
-  typename iMatrix<l,N>::tensor_reduced srhs; srhs=rhs;
-  return lhs*srhs;
-}
-template<class l,int N> strong_inline iMatrix<l,N> operator * (const typename iScalar<l>::scalar_type & lhs,const iMatrix<l,N>& rhs) {  return rhs*lhs; }
-
-////////////////////////////////////////////////////////////////////
-// Double support; cast to "scalar_type" through constructor
-////////////////////////////////////////////////////////////////////
-template<class l> strong_inline iScalar<l> operator * (const iScalar<l>& lhs,double rhs) 
-{
-  typename iScalar<l>::scalar_type t; t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
-  return lhs*srhs;
-}
-template<class l> strong_inline iScalar<l> operator * (double lhs,const iScalar<l>& rhs) {  return rhs*lhs; }
-
-template<class l,int N> strong_inline iVector<l,N> operator * (const iVector<l,N>& lhs,double rhs) 
+template<class l, class r> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<l>::scalar_type,
+r>::value, iScalar<l>>::type operator * (const iScalar<l>& lhs, const r& rhs)
 {
   typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
-  return lhs*srhs;
-}
-template<class l,int N> strong_inline iVector<l,N> operator * (double lhs,const iVector<l,N>& rhs) {  return rhs*lhs; }
-
-template<class l,int N> strong_inline iMatrix<l,N> operator * (const iMatrix<l,N>& lhs,double rhs) 
-{
-  typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
-  return lhs*srhs;
-}
-template<class l,int N> strong_inline iMatrix<l,N> operator * (double lhs,const iMatrix<l,N>& rhs) {  return rhs*lhs; }
-
-////////////////////////////////////////////////////////////////////
-// Complex support; cast to "scalar_type" through constructor
-////////////////////////////////////////////////////////////////////
-template<class l> strong_inline iScalar<l> operator * (const iScalar<l>& lhs,ComplexD rhs) 
-{
-  typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
-  
-  
-  return lhs*srhs;
-}
-template<class l> strong_inline iScalar<l> operator * (ComplexD lhs,const iScalar<l>& rhs) {  return rhs*lhs; }
-
-template<class l,int N> strong_inline iVector<l,N> operator * (const iVector<l,N>& lhs,ComplexD rhs) 
-{
-  typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
-  return lhs*srhs;
-}
-template<class l,int N> strong_inline iVector<l,N> operator * (ComplexD lhs,const iVector<l,N>& rhs) {  return rhs*lhs; }
-
-template<class l,int N> strong_inline iMatrix<l,N> operator * (const iMatrix<l,N>& lhs,ComplexD rhs) 
-{
-  typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
-  return lhs*srhs;
-}
-template<class l,int N> strong_inline iMatrix<l,N> operator * (ComplexD lhs,const iMatrix<l,N>& rhs) {  return rhs*lhs; }
-
-////////////////////////////////////////////////////////////////////
-// Integer support; cast to "scalar_type" through constructor
-////////////////////////////////////////////////////////////////////
-template<class l> strong_inline iScalar<l> operator * (const iScalar<l>& lhs,Integer rhs) 
-{
-  typename iScalar<l>::scalar_type t;  t=rhs;
   typename iScalar<l>::tensor_reduced srhs; srhs=t;
   return lhs*srhs;
 }
-template<class l> strong_inline iScalar<l> operator * (Integer lhs,const iScalar<l>& rhs) {  return rhs*lhs; }
+template<class l, class r> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<r>::scalar_type,
+l>::value, iScalar<r>>::type operator * (const l& lhs, const iScalar<r>& rhs) {  return rhs*lhs; }
 
-template<class l,int N> strong_inline iVector<l,N> operator * (const iVector<l,N>& lhs,Integer rhs) 
+template<class l, class r, int N> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<l>::scalar_type,
+r>::value, iVector<l,N>>::type operator * (const iVector<l,N>& lhs,const r& rhs)
 {
   typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
+  typename iVector<l,N>::tensor_reduced srhs; srhs=t;
   return lhs*srhs;
 }
-template<class l,int N> strong_inline iVector<l,N> operator * (Integer lhs,const iVector<l,N>& rhs) {  return rhs*lhs; }
+template<class l, class r, int N> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<r>::scalar_type,
+l>::value, iVector<r,N>>::type operator * (const l& lhs,const iVector<r,N>& rhs) {  return rhs*lhs; }
 
-template<class l,int N> strong_inline iMatrix<l,N> operator * (const iMatrix<l,N>& lhs,Integer rhs) 
+template<class l, class r, int N> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<l>::scalar_type,
+r>::value, iMatrix<l,N>>::type operator * (const iMatrix<l,N>& lhs,const r& rhs)
 {
   typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
+  typename iMatrix<l,N>::tensor_reduced srhs; srhs=t;
   return lhs*srhs;
 }
-template<class l,int N> strong_inline iMatrix<l,N> operator * (Integer lhs,const iMatrix<l,N>& rhs) {  return rhs*lhs; }
-
-
+template<class l, class r, int N> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<r>::scalar_type,
+l>::value, iMatrix<r,N>>::type operator * (const l& lhs,const iMatrix<r,N>& rhs) {  return rhs*lhs; }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // addition by fundamental scalar type applies to matrix(down diag) and scalar
 ///////////////////////////////////////////////////////////////////////////////////////////////
-template<class l> strong_inline iScalar<l> operator + (const iScalar<l>& lhs,const typename iScalar<l>::scalar_type rhs) 
+template<class l, class r> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<l>::scalar_type,
+r>::value, iScalar<l>>::type operator + (const iScalar<l>& lhs, const r& rhs)
 {
-  typename iScalar<l>::tensor_reduced srhs; srhs=rhs;
-  return lhs+srhs;
-}
-template<class l> strong_inline iScalar<l> operator + (const typename iScalar<l>::scalar_type lhs,const iScalar<l>& rhs) {  return rhs+lhs; }
-
-template<class l,int N> strong_inline iMatrix<l,N> operator + (const iMatrix<l,N>& lhs,const typename iScalar<l>::scalar_type rhs) 
-{
-  typename iMatrix<l,N>::tensor_reduced srhs; srhs=rhs;
-  return lhs+srhs;
-}
-template<class l,int N> strong_inline iMatrix<l,N> operator + (const typename iScalar<l>::scalar_type lhs,const iMatrix<l,N>& rhs) {  return rhs+lhs; }
-
-////////////////////////////////////////////////////////////////////
-// Double support; cast to "scalar_type" through constructor
-////////////////////////////////////////////////////////////////////
-template<class l> strong_inline iScalar<l> operator + (const iScalar<l>& lhs,double rhs) 
-{
-  typename iScalar<l>::scalar_type t; t=rhs;
+  typename iScalar<l>::scalar_type t;t=rhs;
   typename iScalar<l>::tensor_reduced srhs; srhs=t;
   return lhs+srhs;
 }
-template<class l> strong_inline iScalar<l> operator + (double lhs,const iScalar<l>& rhs) {  return rhs+lhs; }
+template<class l, class r> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<r>::scalar_type,
+l>::value, iScalar<r>>::type operator + (const l& lhs, const iScalar<r>& rhs) {  return rhs+lhs; }
 
-template<class l,int N> strong_inline iMatrix<l,N> operator + (const iMatrix<l,N>& lhs,double rhs) 
+template<class l, class r, int N> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<l>::scalar_type,
+r>::value, iMatrix<l,N>>::type operator + (const iMatrix<l,N>& lhs,const r& rhs)
 {
   typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
+  typename iMatrix<l,N>::tensor_reduced srhs; srhs=t;
   return lhs+srhs;
 }
-template<class l,int N> strong_inline iMatrix<l,N> operator + (double lhs,const iMatrix<l,N>& rhs) {  return rhs+lhs; }
-
-
-// Integer support cast to scalar type through constructor
-
-
-template<class l> strong_inline iScalar<l> operator + (const iScalar<l>& lhs,Integer rhs) 
-{
-  typename iScalar<l>::scalar_type t; t=rhs;
-  typename iScalar<l>::tensor_reduced srhs; srhs=t;
-  return lhs+srhs;
-}
-
-template<class l> strong_inline iScalar<l> operator + (Integer lhs,const iScalar<l>& rhs) {  return rhs+lhs; }
-
-template<class l,int N> strong_inline iMatrix<l,N> operator + (const iMatrix<l,N>& lhs,Integer rhs) 
-{
-  typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
-  return lhs+srhs;
-}
-template<class l,int N> strong_inline iMatrix<l,N> operator + (Integer lhs,const iMatrix<l,N>& rhs) {  return rhs+lhs; }
-
+template<class l, class r, int N> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<r>::scalar_type,
+l>::value, iMatrix<r,N>>::type operator + (const l& lhs,const iMatrix<r,N>& rhs) {  return rhs+lhs; }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // subtraction of fundamental scalar type applies to matrix(down diag) and scalar
 ///////////////////////////////////////////////////////////////////////////////////////////////
-template<class l> strong_inline iScalar<l> operator - (const iScalar<l>& lhs,const typename iScalar<l>::scalar_type rhs) 
+template<class l, class r> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<l>::scalar_type,
+r>::value, iScalar<l>>::type operator - (const iScalar<l>& lhs, const r& rhs)
 {
-  typename iScalar<l>::tensor_reduced srhs; srhs=rhs;
-  return lhs-srhs;
-}
-template<class l> strong_inline iScalar<l> operator - (const typename iScalar<l>::scalar_type lhs,const iScalar<l>& rhs) 
-{
-  typename iScalar<l>::tensor_reduced slhs;slhs=lhs;
-  return slhs-rhs;
-}
-
-template<class l,int N> strong_inline iMatrix<l,N> operator - (const iMatrix<l,N>& lhs,const typename iScalar<l>::scalar_type rhs) 
-{
-  typename iScalar<l>::tensor_reduced srhs; srhs=rhs;
-  return lhs-srhs;
-}
-template<class l,int N> strong_inline iMatrix<l,N> operator - (const typename iScalar<l>::scalar_type lhs,const iMatrix<l,N>& rhs) 
-{
-  typename iScalar<l>::tensor_reduced slhs;slhs=lhs;
-  return slhs-rhs;
-}
-
-////////////////////////////////////////////////////////////////////
-// Double support; cast to "scalar_type" through constructor
-////////////////////////////////////////////////////////////////////
-template<class l> strong_inline iScalar<l> operator - (const iScalar<l>& lhs,double rhs) 
-{
-  typename iScalar<l>::scalar_type t; t=rhs;
+  typename iScalar<l>::scalar_type t;t=rhs;
   typename iScalar<l>::tensor_reduced srhs; srhs=t;
   return lhs-srhs;
 }
-template<class l> strong_inline iScalar<l> operator - (double lhs,const iScalar<l>& rhs) 
+template<class l, class r> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<r>::scalar_type,
+l>::value, iScalar<r>>::type operator - (const l& lhs, const iScalar<r>& rhs)
 {
-  typename iScalar<l>::scalar_type t(lhs);
-  typename iScalar<l>::tensor_reduced slhs;slhs=t;
+  typename iScalar<r>::scalar_type t;t=lhs;
+  typename iScalar<r>::tensor_reduced slhs; slhs=t;
   return slhs-rhs;
 }
 
-template<class l,int N> strong_inline iMatrix<l,N> operator - (const iMatrix<l,N>& lhs,double rhs) 
+template<class l, class r, int N> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<l>::scalar_type,
+r>::value, iMatrix<l,N>>::type operator - (const iMatrix<l,N>& lhs,const r& rhs)
 {
   typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
+  typename iMatrix<l,N>::tensor_reduced srhs; srhs=t;
   return lhs-srhs;
 }
-template<class l,int N> strong_inline iMatrix<l,N> operator - (double lhs,const iMatrix<l,N>& rhs) 
+template<class l, class r, int N> strong_inline typename
+std::enable_if<std::is_constructible<typename iScalar<r>::scalar_type,
+l>::value, iMatrix<r,N>>::type operator - (const l& lhs,const iMatrix<r,N>& rhs)
 {
-  typename iScalar<l>::scalar_type t(lhs);
-  typename iScalar<l>::tensor_reduced slhs;slhs=t;
-  return slhs-rhs;
-}
-
-////////////////////////////////////////////////////////////////////
-// Integer support; cast to "scalar_type" through constructor
-////////////////////////////////////////////////////////////////////
-template<class l> strong_inline iScalar<l> operator - (const iScalar<l>& lhs,Integer rhs) 
-{
-  typename iScalar<l>::scalar_type t; t=rhs;
-  typename iScalar<l>::tensor_reduced srhs; srhs=t;
-  return lhs-srhs;
-}
-template<class l> strong_inline iScalar<l> operator - (Integer lhs,const iScalar<l>& rhs) 
-{
-  typename iScalar<l>::scalar_type t;t=lhs;
-  typename iScalar<l>::tensor_reduced slhs;slhs=t;
-  return slhs-rhs;
-}
-template<class l,int N> strong_inline iMatrix<l,N> operator - (const iMatrix<l,N>& lhs,Integer rhs) 
-{
-  typename iScalar<l>::scalar_type t;t=rhs;
-  typename iScalar<l>::tensor_reduced srhs;srhs=t;
-  return lhs-srhs;
-}
-template<class l,int N> strong_inline iMatrix<l,N> operator - (Integer lhs,const iMatrix<l,N>& rhs) 
-{
-  typename iScalar<l>::scalar_type t;t=lhs;
-  typename iScalar<l>::tensor_reduced slhs;slhs=t;
+  typename iScalar<r>::scalar_type t;t=lhs;
+  typename iMatrix<r,N>::tensor_reduced slhs; slhs=t;
   return slhs-rhs;
 }
 
