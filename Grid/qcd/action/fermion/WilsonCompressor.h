6179acfda0b22f26b3b68ea64fa973dcef900327
Peter Boyle <paboyle@ph.ed.ac.uk>
2019-07-14 13:59:54 +0100

    Put  back a call that was required

--- Grid/qcd/action/fermion/WilsonCompressor.h
+++ Grid/qcd/action/fermion/WilsonCompressor.h
@@ -324,7 +324,7 @@ public:
     double t1=usecond();
     // Asynchronous MPI calls multidirectional, Isend etc...
     // Non-overlapped directions within a thread. Asynchronous calls except MPI3, threaded up to comm threads ways.
-    //    this->Communicate();
+    this->Communicate();
     double t2=usecond(); timer1 += t2-t1;
     this->CommsMerge(compress);
     double t3=usecond(); timer2 += t3-t2;
