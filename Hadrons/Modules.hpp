195fcad5e4f6ceb27fefbab0717014221d6b0976
Michael Marshall <43034299+mmphys@users.noreply.github.com>
2019-09-16 18:00:56 +0100

    Drop baryon related modules

--- Hadrons/Modules.hpp
+++ Hadrons/Modules.hpp
@@ -55,10 +55,6 @@
 #include <Hadrons/Modules/MContraction/DiscLoop.hpp>
 #include <Hadrons/Modules/MContraction/A2AAslashField.hpp>
 #include <Hadrons/Modules/MContraction/Meson.hpp>
-#include <Hadrons/Modules/MContraction/Nucleon.hpp>
-#include <Hadrons/Modules/MContraction/SelfContract.hpp>
-#include <Hadrons/Modules/MDistil/BContraction.hpp>
-#include <Hadrons/Modules/MDistil/Baryon2pt.hpp>
 #include <Hadrons/Modules/MDistil/DistilVectors.hpp>
 #include <Hadrons/Modules/MDistil/LapEvec.hpp>
 #include <Hadrons/Modules/MDistil/Noises.hpp>
