ab8318326c7d2ce705f973a53a266c45ed70cacd
Michael Marshall <43034299+mmphys@users.noreply.github.com>
2019-09-16 19:50:37 +0100

    Seems I shouldn't have dropped MContraction/Baryons

--- Hadrons/Modules.hpp
+++ Hadrons/Modules.hpp
@@ -54,6 +54,7 @@
 #include <Hadrons/Modules/MContraction/WeakNonEye3pt.hpp>
 #include <Hadrons/Modules/MContraction/DiscLoop.hpp>
 #include <Hadrons/Modules/MContraction/A2AAslashField.hpp>
+#include <Hadrons/Modules/MContraction/Baryon.hpp>
 #include <Hadrons/Modules/MContraction/Meson.hpp>
 #include <Hadrons/Modules/MDistil/DistilVectors.hpp>
 #include <Hadrons/Modules/MDistil/LapEvec.hpp>
