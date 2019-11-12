78f75b0e9fa6e64aafeb1af841ad6816a33d095b
Michael Marshall <43034299+mmphys@users.noreply.github.com>
2019-11-12 14:00:46 +0000

    Better than graffiti

--- Hadrons/Modules/MDistil/DistilPar.hpp
+++ Hadrons/Modules/MDistil/DistilPar.hpp
@@ -1,4 +1,33 @@
-#ifndef Hadrons_MDistil_DistilPar_hpp_
+/*************************************************************************************
+ 
+ Grid physics library, www.github.com/paboyle/Grid
+ 
+ Source file: Hadrons/Modules/MDistil/DistilPar.hpp
+ 
+ Copyright (C) 2019
+ 
+ Author: Felix Erben <ferben@ed.ac.uk>
+ Author: Michael Marshall <Michael.Marshall@ed.ac.uk>
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ 
+ See the full license in the file "LICENSE" in the top level distribution directory
+ *************************************************************************************/
+/*  END LEGAL */
+
+##ifndef Hadrons_MDistil_DistilPar_hpp_
 #define Hadrons_MDistil_DistilPar_hpp_
 
 #include <Hadrons/Global.hpp>
