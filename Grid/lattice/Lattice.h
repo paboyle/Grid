0e3c49f687c7fdc8398b9c2859053265ed1da121
Peter Boyle <paboyle@ph.ed.ac.uk>
2020-05-12 17:57:01 -0400

    TransposeIndex was broken by Christoph

--- Grid/lattice/Lattice.h
+++ Grid/lattice/Lattice.h
@@ -31,7 +31,7 @@ Author: Peter Boyle <paboyle@ph.ed.ac.uk>
 #include <Grid/lattice/Lattice_ET.h>
 #include <Grid/lattice/Lattice_arith.h>
 #include <Grid/lattice/Lattice_trace.h>
-//#include <Grid/lattice/Lattice_transpose.h>
+#include <Grid/lattice/Lattice_transpose.h>
 #include <Grid/lattice/Lattice_local.h>
 #include <Grid/lattice/Lattice_reduction.h>
 #include <Grid/lattice/Lattice_peekpoke.h>
