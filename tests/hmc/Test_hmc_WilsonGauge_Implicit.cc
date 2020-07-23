873f039c7204d4ad54ae58e1503d37a3f0542e33
Chulwoo Jung <chulwoo@bnl.gov>
2020-07-23 14:45:47 -0400

    Test_hmc_WilsonGauge_Implicit compiles

--- tests/hmc/Test_hmc_WilsonGauge_Implicit.cc
+++ tests/hmc/Test_hmc_WilsonGauge_Implicit.cc
@@ -44,7 +44,7 @@ struct RMHMCActionParameters: Serializable {
 
 int main(int argc, char **argv) {
   using namespace Grid;
-  using namespace Grid::QCD;
+//  using namespace Grid::QCD;
 
   Grid_init(&argc, &argv);
   GridLogIntegrator.Active(1);
@@ -55,7 +55,8 @@ int main(int argc, char **argv) {
    // Typedefs to simplify notation
   typedef GenericHMCRunner<ImplicitMinimumNorm2> HMCWrapper;  // Uses the default minimum norm
    // Serialiser
-  typedef Grid::JSONReader       Serialiser;
+//  typedef Grid::JSONReader       Serialiser;
+  typedef Grid::XmlReader       Serialiser;
 
 
   HMCWrapper TheHMC;
