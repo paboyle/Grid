cda915a3452fe0b8c0613ae3990ce2fc195c12e7
Peter Boyle <paboyle@ph.ed.ac.uk>
2021-10-07 20:29:09 +0100

    Better options

--- systems/Tursa/sourceme.sh
+++ systems/Tursa/sourceme.sh
@@ -1,2 +1,5 @@
-spack load c-lime
 module load cuda/11.4.1 openmpi/4.1.1 ucx/1.10.1
+module load cuda/11.4.1 openmpi/4.1.1 ucx/1.10.1
+export PREFIX=/home/tc002/tc002/shared/env/prefix/
+export LD_LIBRARY_PATH=$PREFIX/lib/:$LD_LIBRARY_PATH
+unset SBATCH_EXPORT
