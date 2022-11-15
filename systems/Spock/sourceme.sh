0352da34f019bea2fd5a7117600f3da68cf40179
Peter Boyle <paboyle@ph.ed.ac.uk>
2022-11-15 16:26:49 -0500

    Several deleted files

--- systems/Spock/sourceme.sh
+++
@@ -1,9 +0,0 @@
-module load emacs
-module load PrgEnv-gnu
-module load rocm/4.5.0
-module load gmp
-module load cray-fftw
-module load craype-accel-amd-gfx908
-export MPIR_CVAR_GPU_EAGER_DEVICE_MEM=0
-export MPICH_GPU_SUPPORT_ENABLED=1
-export LD_LIBRARY_PATH=/opt/cray/pe/gcc/mpfr/3.1.4/lib/:$LD_LIBRARY_PATH
