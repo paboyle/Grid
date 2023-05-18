e855c41772aaffb167bb6c448052737812bdfeb5
Julian Lenz <j.j.lenz@swansea.ac.uk>
2023-05-18 18:09:38 +0100

    Unified spfundamental.h with fundamental.h

--- Grid/qcd/representations/Representations.h
+++ Grid/qcd/representations/Representations.h
@@ -4,7 +4,6 @@
 #include <Grid/qcd/representations/adjoint.h>
 #include <Grid/qcd/representations/two_index.h>
 #include <Grid/qcd/representations/fundamental.h>
-#include <Grid/qcd/representations/spfundamental.h>
 #include <Grid/qcd/representations/sp_two_index.h>
 #include <Grid/qcd/representations/hmc_types.h>
 
