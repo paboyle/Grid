394e506aead163b92186d3a51f47d6059eb1fa7f
Peter Boyle <paboyle@ph.ed.ac.uk>
2024-05-21 02:10:04 +0100

    Compile options for tursa update

--- systems/Tursa/sourceme.sh
+++ systems/Tursa/sourceme.sh
@@ -1,8 +1,7 @@
 module load cuda/12.3 
 module load ucx/1.15.0-cuda12.3  
 module load openmpi/4.1.5-cuda12.3
-
-export PREFIX=/home/tc002/tc002/shared/env/prefix/
-export LD_LIBRARY_PATH=$PREFIX/lib/:$LD_LIBRARY_PATH
+source /home/dp207/dp207/shared/env/production/env-base.sh 
+source /home/dp207/dp207/shared/env/production/env-gpu.sh 
 unset SBATCH_EXPORT
 
