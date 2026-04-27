ce093b2bf3f0e9118f89594a9de68d5aa2bcfc73
Peter Boyle <paboyle@ph.ed.ac.uk>
2026-04-27 13:54:06 -0700

    rdtsc

--- Grid/perfmon/PerfCount.h
+++ Grid/perfmon/PerfCount.h
@@ -51,8 +51,8 @@ Author: paboyle <paboyle@ph.ed.ac.uk>
 #endif
 #ifdef __x86_64__
 #ifdef GRID_CUDA
-accelerator_inline uint64_t __rdtsc(void) {  return 0; }
-accelerator_inline uint64_t __rdpmc(int ) {  return 0; }
+//accelerator_inline uint64_t __rdtsc(void) {  return 0; }
+//accelerator_inline uint64_t __rdpmc(int ) {  return 0; }
 #else
 #include <x86intrin.h>
 #endif
