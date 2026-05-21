b58a1508faa9ef1d25ed2d1711e7183c09e0b266
Peter Boyle <paboyle@ph.ed.ac.uk>
2026-05-21 13:25:13 -0700

    Perlmutter cuda version update

--- systems/Perlmutter/sourceme.sh
+++ systems/Perlmutter/sourceme.sh
@@ -1,4 +1,6 @@
-
 export CRAY_ACCEL_TARGET=nvidia80
+source /global/homes/p/pboyle/spack/share/spack/setup-env.sh
+export MPFR=`spack find --paths mpfr | grep mpfr | cut -c 13-`
+export GMP=`spack find --paths gmp   | grep gmp  | cut -c 12-`
 
-module load PrgEnv-gnu cpe-cuda cudatoolkit/11.4
+module load PrgEnv-gnu cpe-cuda cudatoolkit/12.0
