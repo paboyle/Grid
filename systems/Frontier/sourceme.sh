b914403bbe726753b0b14faf35e73a5b9b52721b
Peter Boyle <paboyle@ph.ed.ac.uk>
2026-05-27 21:31:54 -0400

    Better setup Frontier

--- systems/Frontier/sourceme.sh
+++ systems/Frontier/sourceme.sh
@@ -2,6 +2,10 @@
 echo spack
 . /autofs/nccs-svm1_home1/paboyle/spack/share/spack/setup-env.sh
 
+export CLIME=`spack find --paths c-lime | grep ^c-lime | awk '{print $2}' `
+export MPFR=`spack find --paths mpfr    | grep ^mpfr  | awk '{print $2}' `
+export OPENSSL=`spack find --paths openssl | grep openssl | awk  '{print $2}' `
+export GMP=`spack find --paths gmp      | grep ^gmp | awk '{print $2}' `
 
 module load cce/21.0.0
 module load cpe/26.03
