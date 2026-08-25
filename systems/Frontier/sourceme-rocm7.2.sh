7a292bc4c614f9205e195ce3a2022d9cd12803ce
Peter Boyle <paboyle@ph.ed.ac.uk>
2026-08-25 18:46:18 -0400

    Slate compile fix

--- systems/Frontier/sourceme-rocm7.2.sh
+++ systems/Frontier/sourceme-rocm7.2.sh
@@ -1,7 +1,7 @@
+module unload slate 2>/dev/null
 
 echo spack
 . /autofs/nccs-svm1_home1/paboyle/spack-frontier/share/spack/setup-env.sh
-
 
 export SLATE=`spack find --paths slate   | grep ^slate  | awk '{print $2}' `
 export BLASPP=`spack find --paths blaspp | grep ^blaspp | awk '{print $2}' `
@@ -24,6 +24,8 @@ module load cce/21.0.0
 module load cpe/26.03
 module load rocm/7.2.0
 export LD_LIBRARY_PATH=/opt/rocm-7.2.0/lib/llvm/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LAPACKPP/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$BLASPP/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$SLATE/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CRAY_LD_LIBRARY_PATH
-module load slate
 module load emacs
